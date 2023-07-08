dbdda987569720fd7e32576e590ef9ad1b67b778
AyushMainali123 <ayushmainali11@gmail.com>
2023-07-08 15:47:10 +0545

    feat: updated why choose us section

--- src/components/pages/landing/why-choose-us/WhyChooseUs.tsx
+++ src/components/pages/landing/why-choose-us/WhyChooseUs.tsx
@@ -1,6 +1,7 @@
-import { BodyTwo, HeadingTwo, SubtitleTwo } from '@/components/ui/core/text'
+import { BodyTwo, HeadingTwo, SubtitleOne, SubtitleTwo } from '@/components/ui/core/text'
 import Image from 'next/image'
 import { whyChooseUsData } from './WhyChooseUs.data'
+import EcommerceBookIcon from '/public/images/icons/ecommerce-book.svg'
 import SpecialFeaturesMobile from '/public/images/landing/special-features-mobile.png'
 const WhyChooseUs = () => {
   return (
@@ -20,8 +21,43 @@ const WhyChooseUs = () => {
           </div>
         ))}
       </div>
-      <div>
-        <Image src={SpecialFeaturesMobile} alt="Special Features Mobile" quality={100} />
+      <div className="relative">
+        <Image src={SpecialFeaturesMobile} alt="Special Features Mobile" />
+
+        {/* Consists everything */}
+        <div className="absolute top-[80%] flex flex-col gap-5">
+          {/* First */}
+          <div className="ml-5 flex max-w-max items-center gap-3 rounded-full bg-white p-[10px] pr-5 shadow-[-20px_20px_30px_0px_rgba(106,106,106,0.10)]">
+            <div className="grid h-[38px] w-[38px] place-items-center rounded-full bg-accent-green">
+              <Image src={EcommerceBookIcon} alt="ECommerce Book" width={16} height={16} />
+            </div>
+            <SubtitleOne>60K Student joined</SubtitleOne>
+          </div>
+
+          {/* Second */}
+          <div className="ml-12 flex max-w-max items-center gap-3 rounded-full bg-white p-[10px] pr-5 shadow-[-20px_20px_30px_0px_rgba(106,106,106,0.10)]">
+            <div className="grid h-[38px] w-[38px] place-items-center rounded-full bg-accent-violet">
+              <Image src={EcommerceBookIcon} alt="ECommerce Book" width={16} height={16} />
+            </div>
+            <SubtitleOne>120+ Best Online Course</SubtitleOne>
+          </div>
+
+          {/* Third */}
+          <div className="ml-12 flex max-w-max items-center gap-3 rounded-full bg-white p-[10px] pr-5 shadow-[-20px_20px_30px_0px_rgba(106,106,106,0.10)]">
+            <div className="grid h-[38px] w-[38px] place-items-center rounded-full bg-brand-one">
+              <Image src={EcommerceBookIcon} alt="ECommerce Book" width={16} height={16} />
+            </div>
+            <SubtitleOne>60+ Brands Companions</SubtitleOne>
+          </div>
+
+          {/* Fourth */}
+          <div className="ml-5 flex max-w-max items-center gap-3 rounded-full bg-white p-[10px] pr-5 shadow-[-20px_20px_30px_0px_rgba(106,106,106,0.10)]">
+            <div className="grid h-[38px] w-[38px] place-items-center rounded-full bg-accent-yellow">
+              <Image src={EcommerceBookIcon} alt="ECommerce Book" width={16} height={16} />
+            </div>
+            <SubtitleOne>102+ Experienced Teachers</SubtitleOne>
+          </div>
+        </div>
       </div>
     </section>
   )
